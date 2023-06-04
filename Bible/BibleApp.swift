9baf182386afea2d5a8bba7873b1114118101522
Alexander Spurlock <alexanderspurlock@gmail.com>
2023-06-04 18:37:12 -0400

    Initial Commit

---
+++ Bible/BibleApp.swift
@@ -0,0 +1,17 @@
+//
+//  BibleApp.swift
+//  Bible
+//
+//  Created by Alexander Spurlock on 6/4/23.
+//
+
+import SwiftUI
+
+@main
+struct BibleApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
