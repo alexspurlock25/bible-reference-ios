320e48801525a522cd3da39055c219ef82bf7151
Alexander Spurlock <alexanderspurlock@gmail.com>
2023-06-04 18:51:38 -0400

    basic config

---
+++ Bible/Model/APIBibleConfig.swift
@@ -0,0 +1,12 @@
+//
+//  APIBibleConfig.swift
+//  Bible
+//
+//  Created by Alexander Spurlock on 6/4/23.
+//
+
+import Foundation
+
+struct APIBibleConfig {
+    static let baseURL = "api.scripture.api.bible"
+}
